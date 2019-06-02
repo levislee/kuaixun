9ed1847337ff450f08b3f786309677fa4d6a33bc
lixingxin08 <lixingxin08@163.com>
2019-06-03 02:24:28 +0800

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <app-header></app-header>
-=======
-    <img src="./assets/logo.png">
->>>>>>> 207a23ef61342ae14d444d68a53046d2ac7849cf
     <router-view/>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import appHeader from './components/pages/Header/Header'
 export default {
   name: 'App',
   components:{
     appHeader
-  }
-=======
-export default {
-  name: 'App'
->>>>>>> 207a23ef61342ae14d444d68a53046d2ac7849cf
+  } 
 }
 </script>
 
