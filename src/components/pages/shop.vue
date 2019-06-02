95af5a40ba0c916e2a190a24dafca553d5479d06
levislee <915510049@qq.com>
2019-06-03 01:40:37 +0800

    init

---
+++ src/components/pages/shop.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="slider">
+            <div class="list">
+                <div class="sss" v-for="(item,index) in list" :key="index">{{item}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            list:[0,1,2,3]
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
