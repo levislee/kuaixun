1c7783f7be74e23d1bc49c393cca86fc0417972d
lixingxin08 <lixingxin08@163.com>
2019-06-03 02:26:32 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,14 @@ import Router from 'vue-router'
 // import shop from '../components/pages/shop.vue'
 import { resolve } from 'url';
 const shop=resolve=>require(['@/components/pages/shop'],resolve);
-<<<<<<< HEAD
 const Home=resolve=>require(['@/components/Home'],resolve);
-=======
-const Home=resolve=>require(['@/components/HelloWorld'],resolve);
->>>>>>> 207a23ef61342ae14d444d68a53046d2ac7849cf
 // const HelloWorld=resolve=require(['@/components/HelloWorld'],resolve)
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'HelloWorld', 
       component: Home
     },
     {
